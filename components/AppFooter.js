d16a041a18ae79b99a0c1ecb6a180604015485e1
Tim Cheng <tim.cheng.technine@gmail.com>
2021-10-23 21:55:23 +0800

    update footer

---
+++ components/AppFooter.js
@@ -0,0 +1,42 @@
+import { Row, Col } from 'antd'
+import {
+  TwitterOutlined,
+  MediumOutlined
+} from '@ant-design/icons';
+import Image from './Image'
+
+
+const AppFooter = () => {
+  return (
+    <Row justify="center">
+      <Col span={22}>
+        <Row justify="center" align="middle" style={{ minHeight: 100, textAlign: 'center' }}>
+          <Col xs={4} md={2} style={{ textAlign: 'center' }}>
+            <a href="https://twitter.com/NFT_WSB" target="_blank">
+              <TwitterOutlined style={{ fontSize: 32 }} />
+            </a>
+          </Col>
+          <Col  xs={4} md={2}  style={{ textAlign: 'center' }}>
+            <a href="https://medium.com/@crypto.wallstreetbets" target="_blank">
+              <MediumOutlined style={{ fontSize: 32 }} />
+            </a>
+          </Col>
+          <Col  xs={4} md={2}  style={{ textAlign: 'center', }}>
+            <a href="https://discord.com/invite/p5eNZYa3NY" style={{ textAlign: 'center' }} target="_blank">
+              <div style={{
+                width: 32,
+                height: '100%',
+                textAlign: 'center',
+                margin: 'auto' 
+              }}>
+                <Image src="discord.png" alt="discord" />
+              </div>
+            </a>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  )
+}
+
+export default AppFooter;
