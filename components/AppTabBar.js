a415a934544892d9b0b89f701bf775cb97ad7181
Tim Cheng <tim.cheng.technine@gmail.com>
2021-10-23 21:55:16 +0800

    update tab bar

--- components/AppTabBar.js
+++ components/AppTabBar.js
@@ -1,14 +1,36 @@
-import { Badge, TabBar } from 'antd-mobile';
-import React, { useState } from 'react';
+import { Badge, TabBar, Popover, List } from 'antd-mobile';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router'
 import {
 	AppstoreOutline,
 	FireFill,
 	GlobalOutline,
 	GiftOutline,
+	MoreOutline
 } from 'antd-mobile-icons';
 import defaultStyles from '../core/theme/styles';
 
 const AppTabBar = () => {
+	const router = useRouter()
+	const [activeKey, setActiveKey] = useState('home');
+	const [clicked, setClicked] = useState(false);
+
+	useEffect(() => {
+		if (router.pathname === '/') return setActiveKey('home')
+		if (router.pathname === '/mint') return setActiveKey('mint')
+		if (router.pathname === '/airdrop') return setActiveKey('airdrop')
+		if (router.pathname === '/market') return setActiveKey('marekt')
+		if (router.pathname === '/collection/[id]') return setActiveKey('more')
+	}, [])
+
+	useEffect(() => {
+		if (!clicked) return;
+		if (activeKey === 'home') return router.push('/')
+		if (activeKey === 'mint') return router.push('/mint')
+		if (activeKey === 'airdrop') return router.push('/airdrop')
+		if (activeKey === 'market') return router.push('/market')
+	}, [activeKey, clicked])
+
 	const tabs = [
 		{
 			key: 'home',
@@ -30,12 +52,35 @@ const AppTabBar = () => {
 			title: <span style={defaultStyles.tabBarTitle}>Market</span>,
 			icon: <GlobalOutline />,
 		},
+		{
+			key: 'more',
+			title: <span style={defaultStyles.tabBarTitle}>More</span>,
+			icon: (active) => {
+				return <Popover
+					content={
+						<div>
+							<p>
+								<span style={{ fontSize: 12 }}>More</span>
+							</p>
+							<p>
+								<span style={{ fontSize: 12 }}>Collection</span>
+							</p>
+						</div>
+					}
+					placement='top'
+					mode='light'
+					trigger='click'
+				><MoreOutline /></Popover>
+			},
+		},
 	];
-	const [activeKey, setActiveKey] = useState('home');
 
 	return (
 		<div style={{ position: 'sticky', bottom: 0, background: '#fff' }}>
-			<TabBar activeKey={activeKey} onChange={setActiveKey}>
+			<TabBar activeKey={activeKey} onChange={(value) => {
+				setActiveKey(value);
+				setClicked(true)
+			}}>
 				{tabs.map((item) => (
 					<TabBar.Item key={item.key} icon={item.icon} title={item.title} />
 				))}
