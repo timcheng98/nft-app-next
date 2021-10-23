c26815de7cf22d0b5c44f036a317231dd9fef8ba
Tim Cheng <tim.cheng.technine@gmail.com>
2021-10-23 23:59:20 +0800

    update app layout

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,12 +1,18 @@
 import AppTabBar from './AppTabBar';
 import AppHeader from './AppHeader';
 import AppFooter from './AppFooter';
+import { Row, Col } from 'antd'
 
-const AppLayout = ({ children }) => {
+const AppLayout = ({ children, fullWidth = false }) => {
 	return (
 		<div>
 			<AppHeader />
-			<div style={{ minHeight: '100vh' }}>{children}</div>
+			<Row justify="center">
+				<Col span={fullWidth ? 24 : 22 }>
+					<div style={{ minHeight: '100vh' }}>{children}</div>
+				</Col>
+			</Row>
+
 			<AppFooter />
 			<AppTabBar />
 		</div>
