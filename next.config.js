8ff702811c7d86df523e3c85de268b9287e0da3f
Tim Cheng <timchengy@gmail.com>
2021-10-30 02:18:40 +0800

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -7,5 +7,10 @@ const withTM = require('next-transpile-modules')([
 module.exports = withTM(withImages({
   extends: ["airbnb-base"],
   productionBrowserSourceMaps: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   // 你项目中其他的 Next.js 配置
 }));
