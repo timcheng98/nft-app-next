8aecee669ea99bf29d9131b056f3f6ed9b2c6b36
Tim Cheng <tim.cheng.technine@gmail.com>
2021-11-20 22:56:00 +0800

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name   : "squat-panda",
+    name   : "nft-panda",
     script : "npm --name 'squat-panda' -- start",
     env_production: {
        NODE_ENV: "production"
