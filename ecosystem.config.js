56df915f98c0e0d0ffef75316664c7e63b199433
Tim Cheng <tim.cheng.technine@gmail.com>
2021-11-20 22:44:16 +0800

    udpate

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name   : "panda",
+    name   : "squat-panda",
     script : "npm --name 'squat-panda' -- start",
     env_production: {
        NODE_ENV: "production"
