1c06c8a0918db6f41e06a31a2eafbf2abede09ab
Tim Cheng <tim.cheng.technine@gmail.com>
2021-11-20 22:52:17 +0800

    deploy test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,5 +8,15 @@ module.exports = {
     env_development: {
        NODE_ENV: "development"
     }
-  }]
+  }],
+  deploy: {
+    testing: {
+      "user" : "root",
+      "host" : ["165.22.251.49"],
+      "ref"  : "origin/main-panda",
+      "repo" : "git@github.com:timcheng98/nft-app-next.git",
+      "path" : "/var/www/html/nft-app-next",
+      "post-deploy": 'npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --only nft-panda'
+    }
+  }
 }
